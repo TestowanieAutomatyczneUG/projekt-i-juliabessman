7749f47cf1db46916fd8ee0a7cc0d585f4590252
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 22:16:08 +0100

    Dodanie funkcji pobierz csv

--- src/main.py
+++ src/main.py
@@ -17,3 +17,19 @@ def zapisz_jako_csv(dziennik, test=False):
             e_journal_writer.writerow([student.imie, student.nazwisko, subjects,
                                        student.lista_uwag])
     return 'Zapisano'
+
+def pobierz_csv(dziennik):
+
+    #podaj nazwe csv
+    file_name = input()
+    line_count = 0
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if line_count != 0:
+                dziennik.dodaj_ucznia(row[0], row[1])
+                dziennik.dodaj_przedmiot_do_ucznia(row[0], row[1], row[2])
+                dziennik.dodaj_ocene_do_przedmiotu_ucznia(row[0], row[1], row[2], int(row[3]))
+                dziennik.dodaj_uwage_do_ucznia(row[0], row[1], row[4])
+            line_count += 1
+    return dziennik

8bbe6a503066bf70be0493cb78d5ba0bfc949406
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 22:12:21 +0100

    dodanie funkcji edytuj ocenę

--- src/Przedmiot.py
+++ src/Przedmiot.py
@@ -9,3 +9,19 @@ class Przedmiot:
         if nowa_ocena< 1 or nowa_ocena > 6:
             raise ValueError('Ocena musi mieścić się w przedziale od 1 do 6')
         self.oceny_z_przedmiotu.append(nowa_ocena)
+
+
+    def edytuj_ocene(self, id_oceny, nowa_ocena):
+        if type(nowa_ocena) is not int:
+            raise TypeError('Podany format oceny jest nieprawidłowy')
+        if nowa_ocena< 1 or nowa_ocena > 6:
+            raise ValueError('Ocena musi mieścić się w przedziale od 1 do 6')
+
+        if type(id_oceny) is not int:
+            raise TypeError('Podany format id oceny jest nieprawidłowy')
+
+        if id_oceny < 0 or id_oceny >= len(self.oceny_z_przedmiotu):
+            raise ValueError('Id oceny sie nie zgadza')
+        self.oceny_z_przedmiotu[id_oceny] = nowa_ocena
+
+    

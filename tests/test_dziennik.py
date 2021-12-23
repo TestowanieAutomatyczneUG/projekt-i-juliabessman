6b2d553651b58d1624ecb9921388095ee3bfe35d
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 20:39:25 +0100

    Test dziennik-dodaj ucznia

--- tests/test_dziennik.py
+++ tests/test_dziennik.py
@@ -1,4 +1,5 @@
 import unittest
+
 from hamcrest import *
 from parameterized import parameterized
 from src.Dziennik import Dziennik
@@ -8,7 +9,11 @@ class DziennikTest(unittest.TestCase):
     def setUp(self):
         self.dziennik = Dziennik()
 
+    def test_dodaj_ucznia(self):
+        self.dziennik.dodaj_ucznia('Julia', 'Bessman')
+        assert_that(self.dziennik.lista_uczniow, has_length(1))
 
+   
 
     def tearDown(self):
         self.dziennik = None

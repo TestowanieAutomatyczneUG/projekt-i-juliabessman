f8fb3eec9529bb2b3603d62cd53f8b2b8d502046
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 21:36:48 +0100

    Test uczen-dodaj przedmiot

--- tests/test_uczen.py
+++ tests/test_uczen.py
@@ -9,6 +9,12 @@ from tests.matcher import custom_matcher
 
 
 class UczenTest(unittest.TestCase):
+    def setUp(self):
+        self.uczen = Uczen('Julia', 'Bessman')
+
+    def test_dodaj_przedmiot(self):
+        self.uczen.dodaj_przedmiot('Niemiecki')
+        assert_that(self.uczen.lista_przedmiotow).is_not_none()
 
 
     def tearDown(self):

00a80f5147fbfad9541284610d2f6ef3c22f6136
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 22:07:35 +0100

    Dodanie funkcji dodaj ocenę

--- src/Przedmiot.py
+++ src/Przedmiot.py
@@ -2,3 +2,10 @@ class Przedmiot:
     def __init__(self, przedmiot):
         self.przedmiot = przedmiot
         self.oceny_z_przedmiotu = []
+
+    def dodaj_ocene(self, nowa_ocena):
+        if type(nowa_ocena) is not int:
+            raise TypeError('Podany format oceny jest nieprawidłowy')
+        if nowa_ocena< 1 or nowa_ocena > 6:
+            raise ValueError('Ocena musi mieścić się w przedziale od 1 do 6')
+        self.oceny_z_przedmiotu.append(nowa_ocena)

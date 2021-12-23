d84defa939b9416280ec73ec9ee8f11865d1adbe
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 21:38:29 +0100

    Stworzenie klasy Uczen

---
+++ src/Uczen.py
@@ -0,0 +1,9 @@
+from src.Przedmiot import Przedmiot
+
+
+class Uczen:
+    def __init__(self, imie, nazwisko):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.lista_przedmiotow = []
+        self.lista_uwag = []

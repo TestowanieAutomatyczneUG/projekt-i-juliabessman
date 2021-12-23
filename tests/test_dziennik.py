4a745ce8af7537800dd1d62f341815d8289831fe
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 20:40:29 +0100

    Test dziennik-dodaj ucznia, scenariusze błędne

--- tests/test_dziennik.py
+++ tests/test_dziennik.py
@@ -13,7 +13,23 @@ class DziennikTest(unittest.TestCase):
         self.dziennik.dodaj_ucznia('Julia', 'Bessman')
         assert_that(self.dziennik.lista_uczniow, has_length(1))
 
-   
+    def test_dodaj_ucznia_error_1(self):
+        assert_that(calling(self.dziennik.dodaj_ucznia).with_args(True,'Bessman'), raises(TypeError))
+
+    def test_dodaj_ucznia_error_2(self):
+        assert_that(calling(self.dziennik.dodaj_ucznia).with_args('Julia', True), raises(TypeError))
+
+    def test_dodaj_ucznia_error_3(self):
+        assert_that(calling(self.dziennik.dodaj_ucznia).with_args('', 'Bessman'), raises(ValueError))
+
+    def test_dodaj_ucznia_error_4(self):
+        assert_that(calling(self.dziennik.dodaj_ucznia).with_args('Julia', ''), raises(ValueError))
+
+    def test_dodaj_ucznia_error_istnieje(self):
+        self.dziennik.dodaj_ucznia('Julia', 'Bessman')
+        assert_that(calling(self.dziennik.dodaj_ucznia).with_args('Julia', 'Bessman'), raises(ValueError))
+
+
 
     def tearDown(self):
         self.dziennik = None

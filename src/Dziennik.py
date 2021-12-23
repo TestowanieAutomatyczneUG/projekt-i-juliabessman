492af8e095f4df283186642f411b8b6dcf87bc54
Julia Bessman <julia.bessman@autonomik.pl>
2021-12-23 21:31:46 +0100

    Dodanie funkcji średnia przedmiotu ucznia

--- src/Dziennik.py
+++ src/Dziennik.py
@@ -183,3 +183,20 @@ class Dziennik:
                 return 'Edytowano uwage ucznia'
 
         raise ValueError('Brak ucznia o podanych danych')
+
+    def srednia_przedmiotu_ucznia(self, imie, nazwisko, przedmiot):
+        if type(imie) is not str:
+            raise TypeError('Zmienna "imię" musi być typu string')
+        if type(nazwisko) is not str:
+            raise TypeError('Zmienna "nazwisko" musi być typu string')
+        if len(imie) == 0:
+            raise ValueError('Nie podano imienia')
+        if len(imie) == 0:
+            raise ValueError('Nie podano imienia')
+        if len(nazwisko) == 0:
+            raise ValueError('Nie podano nazwiska')
+
+        for i in range(len(self.lista_uczniow)):
+            if self.lista_uczniow[i].imie == imie and self.lista_uczniow[i].nazwisko == nazwisko:
+                return self.lista_uczniow[i].srednia_przedmiotu(przedmiot)
+        raise ValueError('Brak ucznia o podanych danych')
